391867eeb7878fd41a823cb8d8f7b153ef1e4934
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2023-11-16 20:07:07 +0500

    add: adding regions channel requirements

--- handlers/users/UyJoyHandler.py
+++ handlers/users/UyJoyHandler.py
@@ -30,8 +30,6 @@ async def kvartirasotish(call: types.CallbackQuery):
     await call.message.answer("<b> Категорияни танланг  </b>", reply_markup=button, parse_mode="HTML")
 
 
-
-
 # =======================================1=======================================
 @dp.callback_query_handler(text="andijon", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
@@ -41,146 +39,38 @@ async def kvartirasotish(call: types.CallbackQuery):
 # =======================================2========================================
 @dp.callback_query_handler(text="buxoro", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{buxoroLink}", call.message.chat.id)
-
-    if check_buxoro(get_chat):
-        await call.answer("Buxoro tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=buxoroHome, parse_mode="HTML")
-    else:
-        await bot.send_message(call.message.chat.id,
-                               "Ботдан фойдаланиш учун каналга уланинг.",
-                               parse_mode=mode, reply_markup=button_buxoro())
-
-
-@dp.callback_query_handler(text="check_buxoro", chat_type="private")
-async def check(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{buxoroLink}", call.message.chat.id)
-    if check_buxoro(get_chat):
-        await call.answer("Buxoro tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг! </b>", parse_mode="HTML", reply_markup=buxoroHome)
-    else:
-        await call.answer("Каналга уланмадингиз ! ", show_alert=True)
-
+    await call.answer("Buxoro tanlandi")
+    await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=buxoroHome, parse_mode="HTML")
 
 # =======================================3========================================
 @dp.callback_query_handler(text="fargona", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{fargonaLink}", call.message.chat.id)
-
-    if check_fargona(get_chat):
-        await call.answer("Fargona tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=fargonaHome, parse_mode="HTML")
-    else:
-        await bot.send_message(call.message.chat.id,
-                               "Ботдан фойдаланиш учун каналга уланинг.",
-                               parse_mode=mode, reply_markup=button_fargona())
-
-
-@dp.callback_query_handler(text="check_fargona", chat_type="private")
-async def check(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{fargonaLink}", call.message.chat.id)
-    if check_fargona(get_chat):
-        await call.answer("Fargona tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", parse_mode="HTML", reply_markup=fargonaHome)
-    else:
-        await call.answer("Каналга уланмадингиз ! ", show_alert=True)
-
+    await call.answer("Farg'ona tanlandi")
+    await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=fargonaHome, parse_mode="HTML")
 
 # =======================================4========================================
 @dp.callback_query_handler(text="jizzax", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{jizzaxLink}", call.message.chat.id)
-
-    if check_jizzax(get_chat):
-        await call.answer("Jizzax tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=jizzaxHome, parse_mode="HTML")
-    else:
-        await bot.send_message(call.message.chat.id,
-                               "Ботдан фойдаланиш учун каналга уланинг.",
-                               parse_mode=mode, reply_markup=button_jizzax())
-
-
-@dp.callback_query_handler(text="check_jizzax", chat_type="private")
-async def check(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{jizzaxLink}", call.message.chat.id)
-    if check_jizzax(get_chat):
-        await call.answer("Jizzax tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг! </b>", parse_mode="HTML", reply_markup=jizzaxHome)
-    else:
-        await call.answer("Каналга уланмадингиз ! ", show_alert=True)
-
+    await call.answer("Jizzax tanlandi")
+    await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=jizzaxHome, parse_mode="HTML")
 
 # =======================================5========================================
 @dp.callback_query_handler(text="namangan", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{namanganLink}", call.message.chat.id)
-
-    if check_namangan(get_chat):
-        await call.answer("Namangan tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=namanganHome, parse_mode="HTML")
-    else:
-        await bot.send_message(call.message.chat.id,
-                               "Ботдан фойдаланиш учун каналга уланинг.",
-                               parse_mode=mode, reply_markup=button_namangan())
-
-
-@dp.callback_query_handler(text="check_namangan", chat_type="private")
-async def check(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{namanganLink}", call.message.chat.id)
-    if check_namangan(get_chat):
-        await call.answer("Namangan tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг! </b>", parse_mode="HTML", reply_markup=namanganHome)
-    else:
-        await call.answer("Каналга уланмадингиз ! ", show_alert=True)
-
+    await call.answer("Namangan tanlandi")
+    await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=namanganHome, parse_mode="HTML")
 
 # =======================================6========================================
 @dp.callback_query_handler(text="navoiy", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{navoiyLink}", call.message.chat.id)
-
-    if check_navoiy(get_chat):
-        await call.answer("Navoiy tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=navoiyHome, parse_mode="HTML")
-    else:
-        await bot.send_message(call.message.chat.id,
-                               "Ботдан фойдаланиш учун каналга уланинг.",
-                               parse_mode=mode, reply_markup=button_navoiy())
-
-
-@dp.callback_query_handler(text="check_navoiy", chat_type="private")
-async def check(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{navoiyLink}", call.message.chat.id)
-    if check_navoiy(get_chat):
-        await call.answer("Navoiy tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг! </b>", parse_mode="HTML", reply_markup=navoiyHome)
-    else:
-        await call.answer("Каналга уланмадингиз ! ", show_alert=True)
-
+    await call.answer("Navoiy tanlandi")
+    await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=navoiyHome, parse_mode="HTML")
 
 # =======================================7========================================
 @dp.callback_query_handler(text="qashqadaryo", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{qashqadaryoLink}", call.message.chat.id)
-
-    if check_qashqadaryo(get_chat):
-        await call.answer("Qashqadaryo tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=qashqadaryoHome, parse_mode="HTML")
-    else:
-        await bot.send_message(call.message.chat.id,
-                               "Ботдан фойдаланиш учун каналга уланинг.",
-                               parse_mode=mode, reply_markup=button_qashqadaryo())
-
-
-@dp.callback_query_handler(text="check_qashqadaryo", chat_type="private")
-async def check(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{qashqadaryoLink}", call.message.chat.id)
-    if check_qashqadaryo(get_chat):
-        await call.answer("Qashqadaryo tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг! </b>", parse_mode="HTML", reply_markup=qashqadaryoHome)
-    else:
-        await call.answer("Каналга уланмадингиз ! ", show_alert=True)
-
+    await call.answer("Qashqadaryo tanlandi")
+    await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=qashqadaryoHome, parse_mode="HTML")
 
 # =======================================8========================================
 @dp.callback_query_handler(text="qoraqalpoqosh", chat_type="private")
@@ -191,74 +81,20 @@ async def kvartirasotish(call: types.CallbackQuery):
 # =======================================9========================================
 @dp.callback_query_handler(text="samarqand", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{samarqandLink}", call.message.chat.id)
-
-    if check_samarqand(get_chat):
-        await call.answer("Samarqand tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=samarqandHome, parse_mode="HTML")
-    else:
-        await bot.send_message(call.message.chat.id,
-                               "Ботдан фойдаланиш учун каналга уланинг.",
-                               parse_mode=mode, reply_markup=button_samarqand())
-
-
-@dp.callback_query_handler(text="check_samarqand", chat_type="private")
-async def check(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{samarqandLink}", call.message.chat.id)
-    if check_samarqand(get_chat):
-        await call.answer("Samarqand bosildi")
-        await call.message.answer("<b> Уй-жой турини танланг! </b>", parse_mode="HTML", reply_markup=samarqandHome)
-    else:
-        await call.answer("Каналга уланмадингиз ! ", show_alert=True)
-
+    await call.answer("Samarqand tanlandi")
+    await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=samarqandHome, parse_mode="HTML")
 
 # =======================================10========================================
 @dp.callback_query_handler(text="sirdaryo", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{sirdaryoLink}", call.message.chat.id)
-
-    if check_sirdaryo(get_chat):
-        await call.answer("Sirdaryo tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=sirdaryoHome, parse_mode="HTML")
-    else:
-        await bot.send_message(call.message.chat.id,
-                               "Ботдан фойдаланиш учун каналга уланинг.",
-                               parse_mode=mode, reply_markup=button_sirdaryo())
-
-
-@dp.callback_query_handler(text="check_sirdaryo", chat_type="private")
-async def check(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{sirdaryoLink}", call.message.chat.id)
-    if check_sirdaryo(get_chat):
-        await call.answer("Sirdaryo tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг! </b>", parse_mode="HTML", reply_markup=sirdaryoHome)
-    else:
-        await call.answer("Каналга уланмадингиз ! ", show_alert=True)
-
+    await call.answer("Sirdaryo tanlandi")
+    await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=sirdaryoHome, parse_mode="HTML")
 
 # =======================================11========================================
 @dp.callback_query_handler(text="surxondaryo", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{surxondaryoLink}", call.message.chat.id)
-
-    if check_surxondaryo(get_chat):
-        await call.answer("Surxondaryo tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=surxonHome, parse_mode="HTML")
-    else:
-        await bot.send_message(call.message.chat.id,
-                               "Ботдан фойдаланиш учун каналга уланинг.",
-                               parse_mode=mode, reply_markup=button_surxondaryo())
-
-
-@dp.callback_query_handler(text="check_surxondaryo", chat_type="private")
-async def check(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{surxondaryoLink}", call.message.chat.id)
-    if check_surxondaryo(get_chat):
-        await call.answer("Surxondaryo tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг! </b>", parse_mode="HTML", reply_markup=surxonHome)
-    else:
-        await call.answer("Каналга уланмадингиз ! ", show_alert=True)
-
+    await call.answer("Surxondaryo tanlandi")
+    await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=surxonHome, parse_mode="HTML")
 
 # =======================================12========================================
 @dp.callback_query_handler(text="toshkentsh", chat_type="private")
@@ -275,26 +111,8 @@ async def kvartirasotish(call: types.CallbackQuery):
 # =======================================14========================================
 @dp.callback_query_handler(text="xorazm", chat_type="private")
 async def kvartirasotish(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{xorazmLink}", call.message.chat.id)
-
-    if check_xorazm(get_chat):
-        await call.answer("Xorazm tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=xorazmHome, parse_mode="HTML")
-    else:
-        await bot.send_message(call.message.chat.id,
-                               "Ботдан фойдаланиш учун каналга уланинг.",
-                               parse_mode=mode, reply_markup=button_xorazm())
-
-
-@dp.callback_query_handler(text="check_xorazm", chat_type="private")
-async def check(call: types.CallbackQuery):
-    get_chat = await bot.get_chat_member(f"@{xorazmLink}", call.message.chat.id)
-    if check_xorazm(get_chat):
-        await call.answer("Xorazm tanlandi")
-        await call.message.answer("<b> Уй-жой турини танланг! </b>", parse_mode="HTML", reply_markup=xorazmHome)
-    else:
-        await call.answer("Каналга уланмадингиз ! ", show_alert=True)
-
+    await call.answer("Xorazm tanlandi")
+    await call.message.answer("<b> Уй-жой турини танланг!  </b>", reply_markup=xorazmHome, parse_mode="HTML")
 
 # ===================================--finish--=====================================
 
