4b6268a097db4ea34eb3469cf975b51b4c117624
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2023-12-10 20:09:44 +0500

    edit: change button appearance

--- keyboards/default/JobButton.py
+++ keyboards/default/JobButton.py
@@ -5,7 +5,7 @@ button = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True,
                                  [KeyboardButton(text="УЙ-ЖОЙ БОЗОРИ")],
                              ])
 
-start = ReplyKeyboardMarkup(resize_keyboard=True, row_width=4,
+start = ReplyKeyboardMarkup(resize_keyboard=True, row_width=4, one_time_keyboard=True,
                              keyboard=[
                                  [KeyboardButton(text="START")],
                              ])
